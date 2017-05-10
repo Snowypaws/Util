fa40edc26f2da288c58c496dfea7cf56fe939a06
Alex Metcalfe <a.metcalfe@hotmail.com>
2017-05-10 00:45:29 -0400

    Serialized Class & Dictionary

---
+++ ObservableDictionary.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Util
+{
+    public class ObservableDictionary<TKey, TValue> : IDictionary<TKey, TValue>, INotifyCollectionChanged, INotifyPropertyChanged
+    {
+        private Dictionary<TKey, TValue> _dictionary;
+        
+        public ObservableDictionary()
+        {
+            _dictionary = new Dictionary<TKey, TValue>();
+        } 
+         
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            return _dictionary.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(KeyValuePair<TKey, TValue> item)
+        {
+            var newItems = new List<KeyValuePair<TKey, TValue>>();
+            _dictionary.Add(item.Key, item.Value);
+            newItems.Add(item);
+            CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, newItems));
+        }
+
+        public void Clear()
+        {
+            _dictionary.Clear();
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Count"));
+            CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        }
+
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            return _dictionary.ContainsKey(item.Key);
+        }
+
+        /// <summary>
+        /// Copies the elements of the ICollection<T> to an array, starting at the specified array index.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="arrayIndex"></param>
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            var result = _dictionary.Remove(item.Key);
+            var oldItems = new List<KeyValuePair<TKey, TValue>>();
+
+            if (result)
+            {
+                oldItems.Add(item);
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Count"));
+                CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, oldItems));
+            }
+                
+            return result;
+        }
+
+        public int Count => _dictionary.Count;
+        public bool IsReadOnly => false;
+        public bool ContainsKey(TKey key)
+        {
+            return _dictionary.ContainsKey(key);
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            _dictionary.Add(key, value);
+            var newItems = new List<KeyValuePair<TKey, TValue>>();
+            newItems.Add(new KeyValuePair<TKey, TValue>(key, value));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Count"));
+            CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, newItems));
+        }
+
+        public bool Remove(TKey key)
+        {
+            TValue value;
+            var result = _dictionary.TryGetValue(key, out value);
+            var oldItems = new List<KeyValuePair<TKey, TValue>>();
+            if (result)
+            {
+                oldItems.Add(new KeyValuePair<TKey, TValue>(key, value));
+                _dictionary.Remove(key);
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Count"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Keys"));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Values"));
+                CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, oldItems));
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            return _dictionary.TryGetValue(key, out value);
+        }
+
+        public TValue this[TKey key]
+        {
+            get { return _dictionary[key]; }
+            set
+            {
+                var newItems = new List<KeyValuePair<TKey, TValue>>();
+                var oldItems = new List<KeyValuePair<TKey, TValue>>();
+
+                TValue oldValue;
+                var newValue = value;
+
+
+                _dictionary.TryGetValue(key, out oldValue);
+                
+                if (oldValue == null)
+                {
+                    _dictionary[key] = value; 
+                    newItems.Add(new KeyValuePair<TKey, TValue>(key, newValue));
+
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Count"));
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Keys"));
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Values"));
+                    CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, newItems));
+                }
+                else if (!oldValue.Equals(newValue))
+                {
+                    //oldItems.Add(new KeyValuePair<TKey, TValue>(key, oldValue));
+                    //newItems.Add(new KeyValuePair<TKey, TValue>(key, newValue));
+                    //var changed = new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, newItems,
+                    //    oldItems);
+                    //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Values"));
+                    //CollectionChanged?.Invoke(this, changed);
+
+                    //Remove old key
+                    
+                    if (_dictionary.Remove(key))
+                    {
+                        oldItems.Add(new KeyValuePair<TKey, TValue>(key, oldValue));
+                        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Count"));
+                        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Keys"));
+                        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Values"));
+                        CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove, oldItems));
+                    }
+
+                    //Re-add old key with new value
+
+                    newItems.Add(new KeyValuePair<TKey, TValue>(key, newValue));
+                    _dictionary[key] = newValue;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Count"));
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Keys"));
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Values"));
+                    CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add, newItems));
+                }
+            }
+        }
+
+        public ICollection<TKey> Keys => _dictionary.Keys;
+        public ICollection<TValue> Values => _dictionary.Values;
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ XmlSerializedClass.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace Util
+{
+    public class XmlSerializedClass<T>
+    {
+        [XmlIgnore]
+        public bool Saved { get; private set; }
+
+        public static T Load(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                XmlSerializer reader = new XmlSerializer(typeof(T));
+                FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+
+                T config = default(T);
+
+                try
+                {
+                    config = (T)reader.Deserialize(stream);
+                }
+                catch (Exception e)
+                {
+                    return default(T);
+                }
+
+                if (config is XmlSerializedClass<T>)
+                {
+                    (config as XmlSerializedClass<T>).Saved = true;
+                }
+
+                stream.Close();
+
+                var serialized = config as XmlSerializedClass<T>;
+
+                if (serialized != null)
+                    serialized.OnLoaded();
+
+                return config;
+            }
+
+            return default(T);
+        }
+
+        public static T Load()
+        {
+            return Load(typeof(T).Name + ".cfg");
+        }
+
+        public virtual void Save()
+        {
+            Save(typeof(T).Name + ".cfg");
+        }
+
+        public virtual void OnLoaded()
+        {
+            
+        }
+
+        public virtual bool Read(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                XmlSerializer reader = new XmlSerializer(GetType());
+                FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+
+                object temp = null;
+
+                try
+                {
+                    temp = reader.Deserialize(stream);
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+
+                if (temp != null)
+                {
+                    var properties = temp.GetType().GetProperties();
+
+                    foreach (var property in properties)
+                    {
+                        if (!property.GetCustomAttributes(typeof(XmlIgnoreAttribute), true).Any()
+                            && property.CanWrite && property.CanRead)
+                        {
+                            property.SetValue(this, property.GetValue(temp));
+                        }
+                    }
+
+                    return true;
+                }
+                else
+                    return false;
+            }
+
+            return false;
+        }
+
+        public virtual void Save(string fileName)
+        {
+            XmlSerializer writer = new XmlSerializer(GetType());
+            FileStream stream = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+
+            writer.Serialize(stream, this);
+
+            stream.Close();
+        }
+    }
+}
